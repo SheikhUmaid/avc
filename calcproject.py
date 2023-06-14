67b2fa43e0b7f98589577820e356cb5d4542bdf2
Sheikh Umaid <sheikh.umaid03@gmail.com>
2023-06-14 14:56:52 +0530

    HEKK

---
+++ calcproject.py
@@ -0,0 +1,43 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the fourth number: "))
+num5 = int(input("Enter the sixth number: "))
+num6= int(input("Enter the seventh number: "))
+num7 = int(input("Enter the eight number: "))
+operator = input("Enter the SIGN: ")
+
+if operator == "+":
+  print(num1 + num2+num3+num4+num5+num6+num7)
+elif operator == "-":
+  print(num1 - num2-num3-num4-num5-num6-num7)
+elif operator == "*":
+  print(num1 * num2*num3*num4-num5*num6*num7)
+elif operator == "/":
+  print(num1 / num2/num3/num4/num5/num6/num7)
+elif operator == "<":
+         				  print(num1<num2<num3<num4<num5<num6<num7)
+elif operator == ">":
+  print(num1>num2)
+elif operator == "<=":
+  print(num1<=num2)
+elif operator == ">=":
+  print(num1>=num2)
+elif operator == "π":
+  print(num1πnum2)
+elif operator == "**":
+  print(num1**num2)
+elif operator == "//":
+  print(num1//num2)
+elif operator == "==":
+  print(num1==num2)
+elif operator == "**":
+  print(num1**num2)
+elif operator == "**":
+  print(num1**num2)
+else:
+  print("invalid")
+
+
+
+print("hello wolrd")
